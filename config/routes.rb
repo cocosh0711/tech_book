95b9a322dc63ba171d8b539749acd70e192f2993
cocosh0711 <cocosh0711@gmail.com>
2020-08-29 12:18:11 +0900

    midify_route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'reading_logs/logs'
-  namespace :books do
-    get 'memos/index'
-  end
-  get 'books/index'
   resources :books do
     resources :reading_logs, only: [:index, :create]
     resources :memos
