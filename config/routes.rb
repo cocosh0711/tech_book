162d334a870888e1cd9d733d8e3b0e7a20301f26
tabuchi0919 <tabuchi.igo.0919@gmail.com>
2020-08-29 15:32:03 +0900

    jbuilderを使う

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :books do
-    resources :reading_logs, only: [:index, :create]
-    resources :memos
+  resources :books, defaults: { format: :json } do
+    resources :reading_logs, only: [:index, :create], defaults: { format: :json }
+    resources :memos, defaults: { format: :json }
   end
 end
