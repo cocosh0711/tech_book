ec0d16577b88c1c7c3801d85b469335c26ebf9a3
cocosh0711 <cocosh0711@gmail.com>
2020-08-29 14:09:31 +0900

    modify_memos_and_logs_controller

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,5 +1,21 @@
 class MemosController < ApplicationController
   def index
-    
+    @memos = Memo.all
+    render json: @memos
   end
+
+  def create
+    @memo = Memo.new(
+      book_id: params[:book_id],
+      content: params[:content],
+      page: params[:page]
+    )
+    if @memo.save
+      render json: @memo, status: :created
+    else
+      render json: @memo.errors, status: :unprocessable_entity
+    end
+  end
+
+
 end
